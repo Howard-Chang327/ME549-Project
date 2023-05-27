63861054e6ba4db74f85e03eaf43432a1f595ae0
Howard Chang <120439150+Howard-Chang327@users.noreply.github.com>
2023-05-27 01:29:31 -0700

    make ekf works

--- dynamics_model/ekf.py
+++ dynamics_model/ekf.py
@@ -2,37 +2,25 @@ import numpy as np
 from dynamics_model import *
 
 def EKF(dym,action,mu,state,sigma,dt):
-    '''
-    1. if add v and w in the dynamics, it will crash.
-    2. how do we deal with the C term? it's not introduced in the KF framework.
-    3. when setting covariance too small, it will generate problems
-    '''
-
-
-    # v = np.sqrt(Q)@np.random.randn(n)
-    # w = np.sqrt(R)@np.random.randn(m)
-
-    # X = np.zeros((N,n))  #updated by linearized dynamics
-    # Y = np.zeros((N,m))
     
     #get linearized A and B at each point
     A = dym.A(mu, action, dt)
     B = dym.B(mu, action, dt)
     H = dym.H()
-    # Q = np.diag(np.random.rand(6)) * 0.001
-    # R = np.diag(np.random.rand(2)) * 0.001
-    # w = np.random.rand(2) * 0.001 #size 2 * 1
-    Q = np.eye(6) * 0.01
-    R = np.eye(2) * 0.01
     
-    v = np.random.multivariate_normal(mean=np.zeros(6), cov=Q)
-    # w = np.random.multivariate_normal(mean=np.zeros(2), cov=R)
-    w = np.sqrt(R) @ np.random.normal(0,0.5,2)
-                 
+    m, n = H.shape
     
-    Xhat_pred = A @ mu + B @ action #predicted state
-    P_pred = A @ sigma @ A.T + Q #predicted covariance
-    Y = H @ state + w  #measurement
+    Q = np.eye(n) * 0.01
+    R = np.eye(m) * 0.01
+    
+    v = np.sqrt(Q) @ np.random.normal(0,1,n) # gaussian noise
+    w = np.sqrt(R) @ np.random.normal(0,1,m) # gaussian noise
+    
+    true_traj = state + v # true trajectory = state + noise             
+    
+    Xhat_pred = A @ mu + B @ action # predicted state
+    P_pred = A @ sigma @ A.T + Q # predicted covariance
+    Y = H @ true_traj + w  # measurement = true trajectory + noise 
     
     S = H @ sigma @ H.T + R
     S_inv = np.linalg.inv(S)   
@@ -41,7 +29,7 @@ def EKF(dym,action,mu,state,sigma,dt):
     X_next = Xhat_pred + K @ (Y - H @ Xhat_pred)
     P_next = P_pred - K @ S @ K.T
     
-    return X_next, P_next, Y
+    return X_next, P_next, Y, H @ true_traj
 
 
 
--- dynamics_model/localization.py
+++ dynamics_model/localization.py
@@ -1,88 +1,77 @@
-from dynamics_model import BasePlanarQuadrotor
+from dynamics_model import * 
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
 from ekf import *
 
 if __name__ == '__main__':
     
     dynamics = BasePlanarQuadrotor()
-    
-    # x0 = np.array([0,0,5,0,0,0])
-    # sigma_0 = np.eye(6) * 0.01
-    # path_ideal = [x0]
-    # path_ekf = [x0]
-    # measurement = []
-    # N = 20
-    # dt = 0.17069465800865732
-    
-    # for i in range(19):
-    #     # action = dynamics.control_generate()
-    #     action = dynamics.control_sequence()[i]
-    #     print("action: ", action)
-    #     # ideal path
-    #     init_state_ideal = x0
-    #     ideal_next = (dynamics.discrete_step(init_state_ideal,action,dt))
-    #     path_ideal.append(ideal_next)
-    #     init_state_ideal = ideal_next
-        
-    #     # ekf path
-    #     init_state_ekf = x0
-    #     ekf_next, sigma_next, Y = EKF(dynamics, action, init_state_ekf, sigma_0, dt)
-    #     init_state_ekf = ekf_next
-    #     sigma_0 = sigma_next
-    #     path_ekf.append(ekf_next)
-    #     measurement.append(Y)
 
-    # path_ideal = np.array(path_ideal)
-    # path_ekf = np.array(path_ekf)
-    # measurement = np.array(measurement)
-    
-    # plt.plot(path_ideal[:,0],path_ideal[:,2], color='red', label='ekf')
-    # plt.plot(path_ekf[:,0],path_ekf[:,2], color='blue', label='jacob')
-    # plt.scatter(measurement[:,0],measurement[:,1], color='green', label='measurement')
-    # plt.legend()
-    # plt.show()    
-    
-    
-    #########################
-    
-    x0 = np.array([0,0,5,0,0,0])
-    x0_ekf = np.array([0,0,5,0,0,0])
+    x0 = np.array([0,0,0,0,0,0])
+    x0_ekf = np.array([0,0,0,0,0,0])
     sigma_0 = np.eye(6) * 0.01
     path = [x0]
     path_ekf = [x0_ekf]
     measurement = []
+    true_path = []
     N = 20
-    dt = 0.1
+    dt = 0.12
 
-    for i in range(N):
+    for i in range(N):  
+        
+        # ideal path
         action = dynamics.control_generate()
         x_next = dynamics.discrete_step(path[i],action,dt)
         path.append(x_next)
         x0 = x_next
-        print("x next: ", x_next)
         
         # ekf 
-        ekf_next, sigma_next, Y = EKF(dynamics, action, x0_ekf, x_next, sigma_0, dt)
+        ekf_next, sigma_next, Y, true_traj = EKF(dynamics, action, x0_ekf, x_next, sigma_0, dt)
         path_ekf.append(ekf_next)
         x0_ekf = ekf_next
         sigma_0 = sigma_next
         measurement.append(Y)
+        true_path.append(true_traj)
          
     path = np.array(path)
     path_ekf = np.array(path_ekf)
     measurement = np.array(measurement)
+    true_path = np.array(true_path)
     
-    plt.plot(path[:,0],path[:,2], color='red', label='ideal')
-    plt.plot(path_ekf[:,0],path_ekf[:,2], color='blue', label='ekf')
-    plt.scatter(measurement[:,0],measurement[:,1], color='green', label='measurement')
-    plt.legend()
+    # plt.plot(path[:,0],path[:,2], color='yellow', label='ideal')
+    # # plt.plot(path_ekf[:,0],path_ekf[:,2], color='m', label='ekf')
+    # plt.scatter(measurement[:,0],measurement[:,1], color='pink', label='measurement')
+    # plt.plot(true_path[:,0],true_path[:,1], color='c', label='true')
+    # plt.legend()
+    
+    fig, ax = plt.subplots()
+    
+    ax.plot(path[:,0],path[:,2], color='yellow', label='ideal')
+    ax.scatter(measurement[:,0],measurement[:,1], color='pink', label='measurement')
+    ax.plot(true_path[:,0],true_path[:,1], color='c', label='true')
+    ax.legend()
+    
+    line_ekf, = ax.plot([], [], '*', color='m')  # Node moving along EKF path
+    line_ekf_path, = ax.plot([], [], '-', color='m')  # Path of the moving node
+
+    # Initialization function for the animation
+    def init():
+        ax.set_xlim(np.min(path_ekf[:,0]), np.max(path_ekf[:,0]))
+        ax.set_ylim(np.min(path_ekf[:,2]), np.max(path_ekf[:,2]))
+        return line_ekf, line_ekf_path,
+
+    # Update function for the animation
+    def update(frame):
+        line_ekf.set_data(path_ekf[frame, 0], path_ekf[frame, 2])
+        line_ekf_path.set_data(path_ekf[:frame+1, 0], path_ekf[:frame+1, 2])
+        return line_ekf, line_ekf_path,
+
+    # Create the animation
+    ani = FuncAnimation(fig, update, frames=range(N), init_func=init, blit=True)
+
+    # Show the animation
     plt.show()
-    
-    
-    
-    
-    
         
     
     
